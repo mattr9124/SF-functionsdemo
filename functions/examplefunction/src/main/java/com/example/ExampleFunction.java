06aa70b3a5c183ce166fd07083e39d9b23aacd00
Matt Rossner <mrossner@salesforce.com>
2022-02-28 23:38:19 +0100

    initial commit for Salesforce Functions

---
+++ functions/examplefunction/src/main/java/com/example/ExampleFunction.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import com.salesforce.functions.jvm.sdk.Context;
+import com.salesforce.functions.jvm.sdk.InvocationEvent;
+import com.salesforce.functions.jvm.sdk.SalesforceFunction;
+import com.salesforce.functions.jvm.sdk.data.Record;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.github.benmanes.caffeine.cache.Cache;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Describe ExampleFunction here.
+ */
+public class ExampleFunction implements SalesforceFunction<FunctionInput, FunctionOutput> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleFunction.class);
+
+    private static final String NO_ORG_ID = "000000000000000000";
+    private static final String ERR_NO_ORG = "No Org specified";
+
+    @Override
+    public FunctionOutput apply(InvocationEvent<FunctionInput> event, Context context)
+            throws Exception {
+
+        if (!context.getOrg().isPresent()) {
+            LOGGER.error(ERR_NO_ORG);
+            return new FunctionOutput(Collections.emptyList(), ERR_NO_ORG);
+        }
+
+        if (NO_ORG_ID.equals(context.getOrg().get().getId())) {
+            LOGGER.error(ERR_NO_ORG);
+            return new FunctionOutput(Collections.emptyList(), ERR_NO_ORG);
+        }
+
+        List<Record> records =
+                context.getOrg().get().getDataApi().query("SELECT Id, Name FROM Account").getRecords();
+
+        LOGGER.info("Function successfully queried {} account records!", records.size());
+
+        List<Account> accounts = records.stream()
+                .map(record -> makeAccount(record))
+                .collect(Collectors.toList());
+
+        return new FunctionOutput(accounts);
+    }
+
+    private Account makeAccount(Record record) {
+        return new Account(record.getStringField("Id").get(),
+                record.getStringField("Name").get());
+    }
+}
---
+++ functions/examplefunction/src/main/java/com/example/FunctionOutput.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import java.util.List;
+
+public class FunctionOutput {
+    private final List<Account> accounts;
+    private final String errorMessage;
+
+    public FunctionOutput(List<Account> accounts, String errorMessage) {
+        this.accounts = accounts;
+        this.errorMessage = errorMessage;
+    }
+
+    public FunctionOutput(List<Account> accounts) {
+        this(accounts, null);
+    }
+
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}
