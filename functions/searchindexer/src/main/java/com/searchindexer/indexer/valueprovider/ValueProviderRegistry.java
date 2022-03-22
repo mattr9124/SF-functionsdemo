50c83da2b37a89c2c6b729e681ea71798cfc4f62
Matt Rossner <mrossner@salesforce.com>
2022-03-22 16:20:31 +0100

    use wildcard for value provider type

--- functions/searchindexer/src/main/java/com/searchindexer/indexer/valueprovider/ValueProviderRegistry.java
+++ functions/searchindexer/src/main/java/com/searchindexer/indexer/valueprovider/ValueProviderRegistry.java
@@ -3,10 +3,8 @@ package com.searchindexer.indexer.valueprovider;
 import com.salesforce.functions.jvm.sdk.Org;
 import com.searchindexer.SearchIndexerInput;
 
-import java.util.List;
-
 public interface ValueProviderRegistry {
 
-    ValueProvider<List<String>> getValueProvider(Org org, SearchIndexerInput searchIndexerInput, String valueProvider);
+    ValueProvider<?> getValueProvider(Org org, SearchIndexerInput searchIndexerInput, String valueProvider);
 
 }
--- functions/searchindexer/src/main/java/com/searchindexer/indexer/valueprovider/ValueProviderRegistryImpl.java
+++ functions/searchindexer/src/main/java/com/searchindexer/indexer/valueprovider/ValueProviderRegistryImpl.java
@@ -4,14 +4,13 @@ import com.salesforce.functions.jvm.sdk.Org;
 import com.searchindexer.SearchIndexerInput;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.List;
 
 public class ValueProviderRegistryImpl implements ValueProviderRegistry {
     @Override
-    public ValueProvider<List<String>> getValueProvider(Org org, SearchIndexerInput searchIndexerInput, String valueProvider) {
+    public ValueProvider<?> getValueProvider(Org org, SearchIndexerInput searchIndexerInput, String valueProvider) {
         // TODO build some kind of registry
         try {
-            return (ValueProvider<List<String>>) Class.forName(valueProvider)
+            return (ValueProvider<?>) Class.forName(valueProvider)
                     .getDeclaredConstructor(Org.class, SearchIndexerInput.class).newInstance(org, searchIndexerInput);
         } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException("Unable to create value provider %s".formatted(valueProvider), e);
