99804e40d0577ee6ae4d1b989672634be15f1469
Matt Rossner <mrossner@salesforce.com>
2022-03-22 22:10:30 +0100

    simplified value providers

--- functions/searchindexer/src/main/java/com/searchindexer/indexer/valueprovider/ValueProviderRegistryImpl.java
+++ functions/searchindexer/src/main/java/com/searchindexer/indexer/valueprovider/ValueProviderRegistryImpl.java
@@ -6,11 +6,14 @@ import com.searchindexer.SearchIndexerInput;
 import java.lang.reflect.InvocationTargetException;
 
 public class ValueProviderRegistryImpl implements ValueProviderRegistry {
+
+    private final static String VALUE_PROVIDER_PACKAGE = ValueProviderRegistryImpl.class.getPackageName();
+
     @Override
     public ValueProvider<?> getValueProvider(Org org, SearchIndexerInput searchIndexerInput, String valueProvider) {
         // TODO build some kind of registry
         try {
-            return (ValueProvider<?>) Class.forName(valueProvider)
+            return (ValueProvider<?>) Class.forName(VALUE_PROVIDER_PACKAGE + "." + valueProvider)
                     .getDeclaredConstructor(Org.class, SearchIndexerInput.class).newInstance(org, searchIndexerInput);
         } catch (ClassNotFoundException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException("Unable to create value provider %s".formatted(valueProvider), e);
