d0f5f4f66ce818d0e9a72415c8307f18826cb2f0
Matt Rossner <mrossner@salesforce.com>
2022-04-26 11:41:31 +0200

    got the async example working now

--- force-app/main/default/lwc/fileUploadAsync/fileUploadAsync.js
+++ force-app/main/default/lwc/fileUploadAsync/fileUploadAsync.js
@@ -1,9 +1,19 @@
 import {LightningElement} from 'lwc';
+import {subscribe} from 'lightning/empApi';
 import processFileUpload from '@salesforce/apex/FileUploadHandlerAsync.processFileUpload';
 
 export default class FileUploadAsync extends LightningElement {
 
     parsedResult;
+
+    connectedCallback() {
+        const messageCallback = (response) => {
+            console.log('received event', response);
+            this.parsedResult = response.data.payload.Payload__c;
+        }
+
+        subscribe('/event/UploadEvent__e', -1, messageCallback).then(r => console.log('subscribed', r));
+    }
 
     afterFileUpload(event) {
         const uploadedFiles = event.detail.files;
